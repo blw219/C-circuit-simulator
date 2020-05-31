d253bbecec111898d563e65f014f7a190194ee59
tszwangko <kotszwang@gmail.com>
2020-05-31 04:28:03 -0700

    input test program refined

--- huberts_designs/testinput1.sh
+++ huberts_designs/testinput1.sh
@@ -5,6 +5,6 @@ set -e
     g++ testinput.cpp circuit.cpp -o testinput;
     g++ testprogram.cpp circuit.cpp -o ref;
 
-    ./testinput < testSPICE1.txt > outSPICE1.txt;
-    ./ref > ref.txt;
-    diff ref.txt outSPICE.txt && echo "Success";
+    ./testinput < testSPICE1.txt > outSPICE1.txt &>> outSPICE1.txt &>>outSPICE1.txt;
+    ./ref > ref.txt &>> ref.txt &>> ref.txt;
+    diff ref.txt outSPICE1.txt && echo "Success";
